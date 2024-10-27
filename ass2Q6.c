2aa92d81e16e7b215fedf579e8b90bd28f3d589d
TirtharajDas <tirtharaj.soft@gmail.com>
2024-10-27 13:44:10 +0530

    take data using malloc()

---
+++ ass2Q6.c
@@ -0,0 +1,30 @@
+//Write a program to display n number of elements. Memory should be allocated  dynamically using malloc ( ). 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int *takeData(int n){
+    
+    int *arr = (int*) malloc(n*sizeof(int));
+    printf("Enter the %d element(s):\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    return arr;
+}
+
+int showData(int *data, int n){
+    for(int i=0;i<n;i++){
+        printf("%d",data[i]);
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    printf("Enter no. of elements:\n");
+    scanf("%d",&n);
+    int *data = takeData(n);
+    showData(data,n);
+    return 0;
+}
